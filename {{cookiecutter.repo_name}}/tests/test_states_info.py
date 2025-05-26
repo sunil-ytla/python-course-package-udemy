2cb0c1cd4b4491b8b282f2d042728c95decd0eef
sunil-ytla <113189762+sunil-ytla@users.noreply.github.com>
2025-05-26 19:20:01 +0530

    lint fixes

--- {{cookiecutter.repo_name}}/tests/test_states_info.py
+++ {{cookiecutter.repo_name}}/tests/test_states_info.py
@@ -1,14 +1,11 @@
 """Tests for `example_pkg.states_info`."""
 
-from pathlib import Path
-
 import pytest
 
 from example_pkg.states_info import (
     is_city_capitol_of_state,
     slow_add,
 )
-
 
 @pytest.mark.parametrize(
     argnames=("city_name", "state", "is_capitol"),
